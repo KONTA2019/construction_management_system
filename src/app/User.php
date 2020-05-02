27ae0bbb1f0403164dc2c47288dd6111db2cab38
KONTA2019 <58900062+KONTA2019@users.noreply.github.com>
2020-05-02 23:57:09 +0900

    userテーブル修正

--- src/app/User.php
+++ src/app/User.php
@@ -15,8 +15,12 @@ class User extends Authenticatable
      *
      * @var array
      */
+    // protected $fillable = [
+    //     'name', 'email', 'password',
+    // ];
+
     protected $fillable = [
-        'name', 'email', 'password',
+        'nick_name', 'first_name', 'last_name', 'password', 
     ];
 
     /**
