71fb96de1cd3ca9abb414336bfcefc191bed61e1
KONTA2019 <58900062+KONTA2019@users.noreply.github.com>
2020-05-02 19:08:09 +0900

    reasonテーブル作成

---
+++ src/database/migrations/2020_05_02_100233_create_reasons_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReasonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reasons', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('reason_title')->nullable();
+            $table->string('reason_text')->nullable();
+            $table->integer('operation_amount_id')->unsigned();
+            $table->foreign('operation_amount_id')->references('id')->on('operation_amounts')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reasons');
+    }
+}
